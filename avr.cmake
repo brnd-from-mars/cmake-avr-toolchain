5b692820ea6eb72359115291a9be0dc9cba92837
brendanberg01 <brendan_berg@aol.com>
2020-05-23 13:47:35 +0200

    Add executable search

---
+++ avr.cmake
@@ -0,0 +1,43 @@
+###############################################################################
+# file: avr.cmake
+# author: Brendan Berg
+# MIT License
+# Copyright (c) 2020 Brendan Berg
+###############################################################################
+
+
+###############################################################################
+# find executables
+###############################################################################
+find_program(AVR_GCC avr-gcc)
+find_program(AVR_GXX avr-g++)
+find_program(AVR_SIZE avr-size)
+find_program(AVR_OBJCOPY avr-objcopy)
+find_program(AVR_OBJDUMP avr-objdump)
+find_program(AVRDUDE avrdude)
+
+if(NOT AVR_GCC)
+    message(FATAL_ERROR "Program avr-gcc not found")
+endif(NOT AVR_GCC)
+
+if(NOT AVR_GXX)
+    message(FATAL_ERROR "Program avr-g++ not found")
+endif(NOT AVR_GXX)
+
+if(NOT AVR_SIZE)
+    message(FATAL_ERROR "Program avr-size not found")
+endif(NOT AVR_SIZE)
+
+if(NOT AVR_OBJCOPY)
+    message(FATAL_ERROR "Program avr-objcopy not found")
+endif(NOT AVR_OBJCOPY)
+
+if(NOT AVR_OBJDUMP)
+    message(FATAL_ERROR "Program avr-objdump not found")
+endif(NOT AVR_OBJDUMP)
+
+if(NOT AVRDUDE)
+    message(FATAL_ERROR "Program avrdude not found")
+endif(NOT AVRDUDE)
+
+message(STATUS "AVR toolchain executables search done")
